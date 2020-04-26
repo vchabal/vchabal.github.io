4f9eb5d36c0fcee1d9f512581d6c8d2beee0de74
vchabal <v.chabal@gmail.com>
2020-04-26 14:32:56 +0200

    Adding service worker

---
+++ pbkdf2/sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'pbkdf2-v5';
+const CACHE_FILES = [
+    './',
+    'manifest.webmanifest',
+    'index.html',
+    'pbkdf2.js'
+]
+
+self.addEventListener('install', event =>
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Adding files:', CACHE_FILES);
+                return cache.addAll(CACHE_FILES);
+            })
+    )
+);
+
+self.addEventListener('activate', event => 
+    event.waitUntil(
+        caches.keys().then(cacheNames =>
+            Promise.all(
+                cacheNames.filter(cacheName => {
+                    let delteCache = /^pbkdf2/i.test(cacheName) && cacheName !== CACHE_NAME;
+                    if (!delteCache) console.log('Cache won\'t be deleted:', cacheName);
+                    return delteCache;
+                }).map(cacheName => {
+                    console.log('Deleting cache:', cacheName);
+                    return caches.delete(cacheName);
+                })
+            )
+        )
+    )
+);
+
+self.addEventListener('fetch', event =>
+    event.respondWith(
+        caches.open(CACHE_NAME).then(cache =>
+            cache.match(event.request).then(response => {
+                var fetchPromise = fetch(event.request).then(networkResponse => {
+                    cache.put(event.request, networkResponse.clone());
+                    return networkResponse;
+                });
+                return response || fetchPromise;
+            })
+        )
+    )
+);

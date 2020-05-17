29c0624fd5f7aa9cfa25c9e11ade49453854cc69
vchabal <v.chabal@gmail.com>
2020-05-17 20:58:09 +0200

    Support of input config

--- pbkdf2/pbkdf2.js
+++ pbkdf2/pbkdf2.js
@@ -2,8 +2,8 @@
     
     // Elements
     let form = document.querySelector('form');
-    let salt = document.querySelector('[placeholder=salt]');
-    let password = document.querySelector('[placeholder=password]');
+    let salt = document.querySelector('[type=text]');
+    let password = document.querySelector('[type=password]');
 
     // Reset clipboard and fields
     form.addEventListener('reset', function () {
@@ -15,12 +15,34 @@
         e.stopPropagation();
         e.preventDefault();
 
-        pbkdf2(password.value, salt.value).then(key => setClipboard(build(key)));
+        let config = { len: 16, az: 1, num: 1, sp: 1 };
+        let input = salt.value.trim().split(/\s+/);
+        let saltStr = input.shift();
+        
+        // Parsing parameters from input - len16 az1 num1 sp1
+        input.reduce((o, param) => { setParam(o, param); return o; }, config);
+
+        // Run PBKDF2 and put result into clipboard
+        pbkdf2(password.value, saltStr)
+            .then(key => build(key, config.len, config.az, config.num, config.sp))
+            .then(password => setClipboard(password));
+        
         salt.value = '';
         password.value = '';
 
         return false;
     });
+
+    /**
+     * Adds parameter to the object
+     * @param {object} config Configuration object 
+     * @param {string} param len16, az0, num0, sp0, ...
+     */
+    function setParam(config, param) {
+        let value = param.replace(/^[^\d]+/, '');
+        let prop = param.substr(0, param.length - value.length);
+        config[prop] = parseInt(value);
+    }
 
     /**
      * @param {string} baseKey
@@ -77,28 +99,64 @@
     }
 
     /**
-     * @returns {string} ASCII CHARs from 32 to 126
+     * @returns {Array<string>} ASCII CHARs from 32 to 126
      */
     function getASCII () {
         let asciiChars = [];
         for( let i = 32; i <= 126; i++ ) 
             asciiChars.push(String.fromCharCode(i));
-        return asciiChars.join('');
+        return asciiChars;
     }
 
     /**
      * @param {string} str String
+     * @param {number} length Password length
+     * @param {number} azChars Count of a-z characters
+     * @param {number} numChars Count of 0-9 characters
+     * @param {number} specialChars Count of special characters
      * @returns {string} Generated password
      */
-    function build (str) {
+    function build (str, length, azChars, numChars, specialChars) {
         let chars = getASCII(),
             password = [];
+
         let bi = BigInt('0x'+str),
-            length = BigInt(chars.length);
-        while (password.length < 16) {
-            let c = chars.charAt(Number(bi % length));
+            charsLength = BigInt(chars.length);
+
+        while (password.length < length) {
+            let c = chars[Number(bi % charsLength)];
             password.push(c);
-            bi /= length;
+            bi /= charsLength;
+        }
+
+        // lower + UPPER >= count
+        let azLowerChars = Math.round(azChars/2); 
+            configs = [
+                { regex: /[a-z]/, required: azLowerChars }, 
+                { regex: /[A-Z]/, required: azChars - azLowerChars }, 
+                { regex: /\d/, required: numChars }, 
+                { regex: /\W/, required: specialChars }];
+
+        for (let config of configs) {
+            let currentCnt = password.filter(i => config.regex.test(i)).length;
+            let requiredCnt = config.required;
+
+            if (currentCnt >= requiredCnt) 
+                continue;
+            
+            let subchars = chars.filter(i => config.regex.test(i)),
+                subcharsLength = BigInt(subchars.length);
+
+            for (; currentCnt < requiredCnt; currentCnt++) {
+                let c = subchars[Number(bi % subcharsLength)];
+                bi /= subcharsLength;
+
+                let insertPlaces = BigInt(password.length + 1);
+                let i = Number(bi % insertPlaces);
+                bi /= insertPlaces;
+
+                password.splice(i, 0, c);
+            }
         }
 
         return password.join('');
--- pbkdf2/sw.js
+++ pbkdf2/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pbkdf2-v10';
+const CACHE_NAME = 'pbkdf2-v11';
 const CACHE_FILES = [
     './',
     'index.html',
